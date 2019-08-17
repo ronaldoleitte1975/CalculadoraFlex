ebcc24f02e9edd1f896917b43997d7abccf5f4f5
Logon Aluno <logonaluno@local.com>
2019-08-17 11:46:00 -0300

    Initial commit

---
+++ app/src/main/java/br/com/ronaldo/calculadoraflex/ui/form/FormActivity.kt
@@ -0,0 +1,13 @@
+package br.com.ronaldo.calculadoraflex.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.ronaldo.calculadoraflex.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ app/src/main/java/br/com/ronaldo/calculadoraflex/ui/result/ResultActivity.kt
@@ -0,0 +1,13 @@
+package br.com.ronaldo.calculadoraflex.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.ronaldo.calculadoraflex.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
